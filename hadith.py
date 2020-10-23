b4c09458ab8fa6779d827582548526b64c71b94e
rbrtjns90 <rbrt.jns90@gmail.com>
2020-10-23 12:54:46 -0600

    Update hadith.py
    Did clean up

--- hadith.py
+++ hadith.py
@@ -5,18 +5,22 @@ from docx import Document
 import time
 from selenium import webdriver
 
-'''nawawi40, qudsi40, riyadussalihin, mishkat
+'''
+Hadith with a one page structure
+
+nawawi40, qudsi40, riyadussalihin, mishkat
+
 '''
 hadith_books = {"bukhari": 97, "muslim": 56, "nasai": 51, "abudawud": 43,
                 "tirmidhi": 49, "ibnmajah": 37, "malik": 61, "ahmad": 71,
                 "adab": 57, "shamail": 56, "bulugh": 16,
                 }
 
-#hadith_books = {"bukhari": 97}
 url = 'https://sunnah.com/'
-headers={'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'}
+# Can alternatively use requests or selenium if blocked from scraping activity
 
-driver = webdriver.Chrome() #Comment allm webdriver elements and uncomment requests to switch
+driver = webdriver.Chrome() #Comment webdriver elements and uncomment requests to use request (Will get a speed boost)
+
 for book, section in hadith_books.items():
     print("Book and Section")
     print(book, section)
@@ -25,9 +29,12 @@ for book, section in hadith_books.items():
         built_url = str(url) + str(book) + "/" + str(i)
         print(built_url)
         driver.get(built_url) 
+        #headers={'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'}
         #r = requests.get(built_url, headers=headers)
         #page_data = r.text
         #print(r.text)
+        #soup = BeautifulSoup(r.text, "html.parser")
+        
         soup = BeautifulSoup(driver.page_source, "html.parser")
         document = Document()
 
